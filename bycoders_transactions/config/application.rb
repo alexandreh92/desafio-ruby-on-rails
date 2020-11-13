b84360415066d1bc47b824f777870e894e84acdb
Alexandre Stapenhorst <eng.alexandreh@gmail.com>
2020-11-13 20:08:11 -0300

    chore(global): added testing gems to Gemfile.lock

--- bycoders_transactions/config/application.rb
+++ bycoders_transactions/config/application.rb
@@ -11,6 +11,13 @@ module BycodersTransactions
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.test_framework nil
+    end
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :"pt-BR"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
