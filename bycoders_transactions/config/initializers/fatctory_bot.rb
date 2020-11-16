4906ceaf387b3b54848ea3245130ea50205b328f
Alexandre Stapenhorst <eng.alexandreh@gmail.com>
2020-11-16 03:53:35 -0300

    build: changed some configs

--- bycoders_transactions/config/initializers/fatctory_bot.rb
+++ bycoders_transactions/config/initializers/fatctory_bot.rb
@@ -1,12 +1,14 @@
-require 'factory_bot'
-
-module FactoryBot
-  module Syntax
-    module Methods
-      def find_or_create(name, attributes = {}, &block)
-        first = FactoryBot::Internal.factory_by_name(name).build_class.first
-        first || FactoryBot.create(name, attributes, &block)
+if Rails.env.test?
+  require 'factory_bot'
+    module FactoryBot
+    module Syntax
+      module Methods
+        def find_or_create(name, attributes = {}, &block)
+          first = FactoryBot::Internal.factory_by_name(name).build_class.first
+          first || FactoryBot.create(name, attributes, &block)
+        end
       end
     end
   end
 end
+
